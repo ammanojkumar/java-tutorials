0d6a90a2e7beaf68b94893155992f3852ba0871d
Manoj Kumar <a.m.manojkumar@gmail.com>
2020-10-03 18:02:26 +0530

    maven archetype

---
+++ maven-springboot-archetype/src/main/resources/archetype-resources/src/main/java/__app-name__.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ${app-name} {
+
+	public static void main(String[] args) {
+		SpringApplication.run(${app-name}.class, args);
+	}
+
+}
